2a9a3ec04ed2fff525f4e6c446240b2383a47ea0
ElonBytyqi <elon.bytyqi@hotmail.com>
2023-03-20 15:33:16 +0100

    register

--- in-Touch/src/pages/Register.tsx
+++ in-Touch/src/pages/Register.tsx
@@ -1,8 +1,50 @@
+import './Login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FormControl, Button ,Image} from 'react-bootstrap';
+import { CiUser } from "react-icons/ci";
+
 function Register(){
     return(
-        <div>
-            <h1>Register Page</h1>
-        </div>
+    <div className="login">
+            <div className="form">
+                <form noValidate>
+                    <CiUser className='login-icon' />
+                    <span>Sign up</span>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Enter your name"
+                        className="form-control inp_text"
+                        id="name" />
+                    <input
+                        type="text"
+                        name="surname"
+                        placeholder="Enter your surname"
+                        className="form-control inp_text"
+                        id="surname" />
+
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter your email"
+                        className="form-control inp_text"
+                        id="email" />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        className="form-control" />
+
+                    <Button variant="outline-primary" className='butoni-post'>Register</Button>
+                    <p>If you already have an Account?<a href='/login'>Login here!</a></p>
+                </form>
+             
+            </div>
+        
+
+            </div>
+           
+        
     )
 }
 export default Register;
