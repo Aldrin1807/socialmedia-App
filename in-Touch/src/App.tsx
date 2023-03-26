3677cc7012ac5b9c40559ad028f8f670be4d28d2
Aid Maliqi <aid.maliqi04@gmail.com>
2023-03-26 18:42:07 +0200

    Aids commit

--- in-Touch/src/App.tsx
+++ in-Touch/src/App.tsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import PageNotFound from './pages/PageNotFound'
-import Profile from './pages/Profile'
-import Register from './pages/Register'
-
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import { EditProfile } from "./pages/EditProfile";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route index element={<Navigate to='/home' />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='*' element={<PageNotFound />} />
+        <Route index element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/editProfile" element={<EditProfile />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
