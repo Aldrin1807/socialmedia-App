e812ccd8030a1f8e4059e5aeced5643e567f077d
Aid Maliqi <aid.maliqi04@gmail.com>
2023-05-12 01:01:47 +0200

    test3

--- in-Touch/src/components/Modals/Modals.tsx
+++ in-Touch/src/components/Modals/Modals.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   MDBBtn,
   MDBModal,
@@ -8,72 +8,91 @@ import {
   MDBModalTitle,
   MDBModalBody,
   MDBModalFooter,
-} from 'mdb-react-ui-kit';
-import axios from 'axios';
+  MDBInput,
+} from "mdb-react-ui-kit";
+import axios from "axios";
 
-export function DeletePost(props:any){
-
-      const toggleShow = () => props.setDeleteModal(!props.deleteModal);
-      const handleDelete = () =>{
-        axios.delete(props.deleteUrl);
-        toggleShow();
-      }
-    return (
-      <>
-      <MDBModal show={props.deleteModal} setShow={props.setDeleteModal} tabIndex='-1'>
-          <MDBModalDialog>
-            <MDBModalContent>
-              <MDBModalHeader>
-                <MDBModalTitle>Are you sure you want to delete this Post</MDBModalTitle>
-                <MDBBtn className='btn-close' color='none' onClick={toggleShow}></MDBBtn>
-              </MDBModalHeader>
-              <MDBModalFooter>
-                <MDBBtn color='primary' onClick={toggleShow}>
-                  Cancel
-                </MDBBtn>
-                <MDBBtn color='danger' onClick={handleDelete}>Delete</MDBBtn>
-              </MDBModalFooter>
-            </MDBModalContent>
-          </MDBModalDialog>
-        </MDBModal>
-      </>
-    );
+export function DeletePost(props: any) {
+  const toggleShow = () => props.setDeleteModal(!props.deleteModal);
+  const handleDelete = () => {
+    axios.delete(props.deleteUrl);
+    toggleShow();
+  };
+  return (
+    <>
+      <MDBModal
+        show={props.deleteModal}
+        setShow={props.setDeleteModal}
+        tabIndex="-1"
+      >
+        <MDBModalDialog>
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle>
+                Are you sure you want to delete this Post
+              </MDBModalTitle>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalFooter>
+              <MDBBtn color="primary" onClick={toggleShow}>
+                Cancel
+              </MDBBtn>
+              <MDBBtn color="danger" onClick={handleDelete}>
+                Delete
+              </MDBBtn>
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+    </>
+  );
 }
 
+export function Modal({ centredModal, setCentredModal, props }: any) {
+  //   const [centredModal, setCentredModal] = useState(false);
+  const [modalData, setModalData] = useState({
+    name: "",
+    type: "",
+    value: "",
+  });
+  const toggleShow = (data: any) => {
+    setCentredModal(!centredModal), setModalData(data);
+  };
 
-export function Modal({centredModal, setCentredModal}: any) {
-    //   const [centredModal, setCentredModal] = useState(false);
-    
-      const toggleShow = () => setCentredModal(!centredModal);
-      return (
-        <>
-          <MDBModal tabIndex="-1" show={centredModal} setShow={setCentredModal}>
-            <MDBModalDialog centered>
-              <MDBModalContent>
-                <MDBModalHeader>
-                  <MDBModalTitle>Modal title</MDBModalTitle>
-                  <MDBBtn
-                    className="btn-close"
-                    color="none"
-                    onClick={toggleShow}
-                  ></MDBBtn>
-                </MDBModalHeader>
-                <MDBModalBody>
-                  <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo
-                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                    risus, porta ac consectetur ac, vestibulum at eros.
-                  </p>
-                </MDBModalBody>
-                <MDBModalFooter>
-                  <MDBBtn color="secondary" onClick={toggleShow}>
-                    Close
-                  </MDBBtn>
-                  <MDBBtn>Save changes</MDBBtn>
-                </MDBModalFooter>
-              </MDBModalContent>
-            </MDBModalDialog>
-          </MDBModal>
-        </>
-      );
-    }
+  return (
+    <>
+      <MDBModal tabIndex="-1" show={centredModal} setShow={setCentredModal}>
+        <MDBModalDialog centered>
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle>Change {modalData.name} </MDBModalTitle>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalBody>
+              <MDBInput
+                label="Password input"
+                id="typePassword"
+                type={modalData.type}
+                value={modalData.value}
+              />
+            </MDBModalBody>
+            <MDBModalFooter>
+              <MDBBtn color="secondary" onClick={toggleShow}>
+                Close
+              </MDBBtn>
+              <MDBBtn>Save changes</MDBBtn>
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
+    </>
+  );
+}
