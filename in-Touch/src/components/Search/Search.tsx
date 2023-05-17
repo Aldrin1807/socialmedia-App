6e6db90e5684e237f44f84cf830fd99c588758f8
Aldrin1807 <ai58914@ubt-uni.net>
2023-05-17 19:20:43 +0200

    search ui modified

--- in-Touch/src/components/Search/Search.tsx
+++ in-Touch/src/components/Search/Search.tsx
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 function Search (props:any){
-    const user = localStorage.getItem("UserId") ?? sessionStorage.getItem("UserId");
     const params = new URLSearchParams(window.location.search);
     const query =  params.get("query");
     const token = sessionStorage.getItem("token")
@@ -34,28 +33,28 @@ function Search (props:any){
     },[userData])
     
     return(
-        <div className='container' id='search-container'>
-                        <p className='lead'>Showing results for " {query} "</p>
-                        <ul className="friend-list clearfix" id='lista'>
-                        {userData.length === 0 ? (
-                            <p>No users found.</p>
-                        ) : (
-                            userData.map((user) => (
-                            <li key={user.id}>
-                                <a href={`/profile?user=${user.id}`}>
-                                <div className="friend-img">
-                                    <img src={`https://localhost:44386/User Images/${user.imagePath}`} alt="" />
-                                </div>
-                                <div className="friend-info">
-                                    <h4>{user.firstName + " " + user.lastName}</h4>
-                                    <p>@{user.username}</p>
-                                </div>
-                                </a>
-                            </li>
-                            ))
-                        )}
-                        </ul>
+            <div className='container' id='search-container'>
+            <p className='lead'>Showing results for "{query}"</p>
+            {userData.length === 0 ? (
+            <p style={{textAlign:'center',fontSize:'large',textDecoration:'underline'}}>No users found.</p>
+            ) : (
+            <ul className="friend-list clearfix" id='lista'>
+                {userData.map((user) => (
+                <li key={user.id} className='item'>
+                    <a href={`/profile?user=${user.id}`}>
+                    <div className="friend-img">
+                        <img src={`https://localhost:44386/User Images/${user.imagePath}`} alt="" />
                     </div>
+                    <div className="friend-info">
+                        <h4>{user.firstName + " " + user.lastName}</h4>
+                        <p>@{user.username}</p>
+                    </div>
+                    </a>
+                </li>
+                ))}
+            </ul>
+            )}
+        </div>
     
 
     )
