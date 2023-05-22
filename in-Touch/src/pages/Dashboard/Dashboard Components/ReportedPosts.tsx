c2a1c4bbd8330c96928f107dd405aae06a9e1d9b
Aldrin1807 <ai58914@ubt-uni.net>
2023-05-22 17:31:20 +0200

    Got the reports from the api, keep post handle on report.

--- in-Touch/src/pages/Dashboard/Dashboard Components/ReportedPosts.tsx
+++ in-Touch/src/pages/Dashboard/Dashboard Components/ReportedPosts.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Reports from './Reports'
+import axios from 'axios'
 
 export const ReportedPosts = () => {
+  const [reports,setReports] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://localhost:44386/api/Reports/get-reports')
+    .then((response:any)=>{
+      setReports(response.data);
+    })
+  },[reports])
+
+
   return (
     <div>
-      <Reports />
+      {reports.map((report:any)=>(
+        <Reports postId={report.postId} userId={report.userId} />
+      ))}
     </div>
   )
 }
--- in-Touch/src/pages/Dashboard/Dashboard Components/Reports.tsx
+++ in-Touch/src/pages/Dashboard/Dashboard Components/Reports.tsx
@@ -2,21 +2,83 @@ import { Button } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
 import Post from '../../../components/Post/Posts';
 import './Reports.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import swal from "sweetalert";
+function Reports(props:any){
 
-function Reports(){
+  const [postData,setPostData] = useState({
+    id:'',
+    content:'',
+    imagePath:"",
+    madeBy: ''
+  })
+
+
+    useEffect(() => {
+      axios.get(`https://localhost:44386/api/Posts/get-post-info?postId=${props.postId}`)
+      .then((response:any)=>{
+        setPostData({
+          id:response.data.id,
+          content:response.data.content,
+          imagePath:response.data.imagePath,
+          madeBy:response.data.userID
+        })
+      })
+
+    }, [props.postId])
+
+    const handleDeleteReport = () => {
+      swal({
+        title: 'Are you sure?',
+        text: 'Once deleted, the report cannot be recovered!',
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true,
+      })
+        .then((confirmed) => {
+          if (confirmed) {
+            axios
+              .delete('https://localhost:44386/api/Reports/delete-report', {
+                data: {
+                  userId: props.userId,
+                  postId: props.postId,
+                },
+              })
+              .then((response:any) => {
+                if(response.data.status=="Success"){
+                  swal("Post successfully kept!"," ", "success")
+                }else{
+                  swal(`${response.data.message}`," ", "Error")
+                }
+              })
+          }
+        })
+    };
+  
+    const handleDeletePost =() =>{
+
+    }
+    const handleLockUser=()=>{
+
+    }
+    
+
     return(
         <>
-        <Accordion>
+      <Accordion>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>User with ID #1 has reported Post #2</Accordion.Header>
+        <Accordion.Header>User with ID #{props.userId} has reported Post #{props.postId}</Accordion.Header>
         <Accordion.Body>
         <div className='report-content'>
-        <h2>Post Content</h2>
-        <img src="https://image-placeholder.com/images/actual-size/200x200.png" style={{width:"200px",height:"200px"}} alt="" />
-        <p>post content</p>
+        <h2>Post #{props.postId}</h2>
+        {postData.imagePath &&
+        <img  src={`https://localhost:44386/Post Images/${postData.imagePath}`} style={{width:"200px",height:"200px"}} alt="" /> 
+        }
+        <p>{postData.content}</p>
         </div>
         <div className='buttons' > 
-        <Button className='reports-btn btn-primary'>Keep Post</Button>
+        <Button className='reports-btn btn-primary' onClick={handleDeleteReport}>Keep Post</Button>
         <Button className='reports-btn btn-danger'>Delete Post</Button>
         <Button className='reports-btn btn-danger'>Lock this User</Button>
         </div>
